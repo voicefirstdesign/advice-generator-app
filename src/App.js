e9ef955b6e1ec4acaf40bce77e1cccd81c1d171c
voicefirstdesign <fab@voicefirstdesign.com>
2022-08-28 21:01:30 +1000

    API Call Feature Added
    The api returns advice on button click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function App(){
+
+  const [currentAdvice, setCurrentAdvice] = React.useState({})
+  
+  const getAdvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+    .then(res => res.json())
+    .then(data => {
+      setCurrentAdvice(data.slip)
+      
+    })
+    console.log(currentAdvice)
+    console.log("You clicked me for adivce")
+    
+    
+  }
+
+
+
+  console.log(currentAdvice)
+
+  
+ 
+
+ 
   
   
   return(
-    <p>hello world</p>
+    <>
+      <p>{currentAdvice.advice}</p>
+      <button onClick={() => getAdvice()}>get advice</button>
+    </>
   )
+
 }
