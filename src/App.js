f4e41b584ba879717a6ced1c729f3f5d77bc3d11
voicefirstdesign <fab@voicefirstdesign.com>
2022-09-18 18:08:25 +1000

    Finished Styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
+import "./App.css"
+import { BsFillDice5Fill } from "react-icons/bs"
 
 export default function App(){
 
-  const [currentAdvice, setCurrentAdvice] = React.useState({})
+  const [currentAdvice, setCurrentAdvice] = React.useState({advice: "click for advice"})
   
   const getAdvice = () => {
     fetch('https://api.adviceslip.com/advice')
@@ -11,27 +13,24 @@ export default function App(){
       setCurrentAdvice(data.slip)
       
     })
-    console.log(currentAdvice)
-    console.log("You clicked me for adivce")
-    
-    
+
+  
+  
   }
 
-
-
-  console.log(currentAdvice)
-
-  
- 
-
- 
-  
-  
   return(
-    <>
-      <p>{currentAdvice.advice}</p>
-      <button onClick={() => getAdvice()}>get advice</button>
-    </>
+    <div className="app-container">
+      <div className="advice-container">
+        {currentAdvice.id && <p className="advice-number">Advice #{currentAdvice.id}</p>}
+        <p className="advice-text">{currentAdvice.advice}</p>
+        <div className="quote-container">
+          <hr></hr><h3> '' </h3><hr></hr>
+        </div>
+        <div className="button-container">
+          <button className="btn-get-advice" onClick={() => getAdvice()}><BsFillDice5Fill /></button>
+        </div>
+      </div>
+    </div>
   )
 
 }
